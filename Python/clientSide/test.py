f93f3bd2470cd8a445c8b45c85f9f1afa6c239a8
Michael Rutkowski <mchlrtkwski@gmail.com>
2017-03-30 02:12:53 -0400

    Added a Test Script to test alerts
    Added a Test Script to test alerts

---
+++ Python/clientSide/test.py
@@ -0,0 +1,28 @@
+import subprocess
+import time
+from uuid import getnode as get_mac
+from socket import *
+
+##########################################################################
+#
+#
+#
+#
+#
+#
+#
+#
+##########################################################################
+
+#Initalize Constants
+MAC = str(get_mac())
+TCP_IP = '184.57.165.207'
+TCP_PORT = 5005
+BUFFER_SIZE = 1024
+
+s = socket(AF_INET, SOCK_STREAM)
+s.connect((TCP_IP, TCP_PORT))
+s.send("1233455")
+data = s.recv(BUFFER_SIZE)
+s.close()
+print "received data:", data
