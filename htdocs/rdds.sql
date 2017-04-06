1baa2fe97ff734c9bb530fa70a7045d50ff31f7e
bryanriik <boyer.59@wright.edu>
2017-04-06 15:25:10 -0400

    Updated script

--- htdocs/rdds.sql
+++ htdocs/rdds.sql
@@ -1,69 +1,61 @@
--- phpMyAdmin SQL Dump
--- version 4.5.2
--- http://www.phpmyadmin.net
---
--- Host: localhost
--- Generation Time: Mar 16, 2017 at 06:07 PM
--- Server version: 10.1.16-MariaDB
--- PHP Version: 5.6.24
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS devices;
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
+CREATE TABLE users (
+  id int(10) NOT NULL,
+  email varchar(255) NOT NULL,
+  password char(64) NOT NULL,
+  salt char(16) NOT NULL,
+  deviceID varchar(64) NOT NULL,
+  log varchar(5000) DEFAULT NULL,
+  phone varchar(255) DEFAULT NULL,
+  alertMethod varchar(10) DEFAULT 'none',
+  carrier varchar(64) DEFAULT NULL
+);
 
+ 
+CREATE TABLE devices ( 
+    id varchar(255) NOT NULL
+    );
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+ALTER TABLE users
+  ADD PRIMARY KEY (id);
 
---
--- Database: `rdds`
---
+ALTER TABLE users
+  MODIFY id int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
--- --------------------------------------------------------
+ALTER TABLE devices
+  ADD PRIMARY KEY (id);
 
---
--- Table structure for table `users`
---
+ALTER TABLE users
+  ADD FOREIGN KEY (deviceID) REFERENCES devices(id);
+  DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS devices;
 
-CREATE TABLE `users` (
-  `id` int(10) NOT NULL,
-  `email` varchar(255) NOT NULL,
-  `password` char(64) NOT NULL,
-  `salt` char(16) NOT NULL,
-  `deviceID` varchar(64) NOT NULL,
-  `log` varchar(5000) DEFAULT NULL,
-  `phone` varchar(255) DEFAULT NULL,
-  `alertMethod` varchar(10) DEFAULT 'Email',
-  `carrier` varchar(64) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+CREATE TABLE users (
+  id int(10) NOT NULL,
+  email varchar(255) NOT NULL,
+  password char(64) NOT NULL,
+  salt char(16) NOT NULL,
+  deviceID varchar(64) NOT NULL,
+  log varchar(5000) DEFAULT NULL,
+  phone varchar(255) DEFAULT NULL,
+  alertMethod varchar(10) DEFAULT 'none',
+  carrier varchar(64) DEFAULT NULL
+);
 
---
--- Dumping data for table `users`
---
+ 
+CREATE TABLE devices ( 
+    id varchar(255) NOT NULL
+    );
 
-INSERT INTO `users` (`id`, `email`, `password`, `salt`, `deviceID`, `log`, `phone`, `alertMethod`, `carrier`) VALUES
-(1, 'mchlrtkwski@gmail.com', 'f55195d8a369d84b6b8e8853a6b7395f58613289bc6025780ab89464f61b9c93', '7b2910c328f8663d', '1233455', '', '9379795219', 'Text', '@mymetropcs.com');
+ALTER TABLE users
+  ADD PRIMARY KEY (id);
 
---
--- Indexes for dumped tables
---
+ALTER TABLE users
+  MODIFY id int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
---
--- Indexes for table `users`
---
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `users`
---
-ALTER TABLE `users`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+ALTER TABLE devices
+  ADD PRIMARY KEY (id);
+  
+INSERT INTO devices ( id ) VALUES ('B8:27:EB:33:B4:83')
